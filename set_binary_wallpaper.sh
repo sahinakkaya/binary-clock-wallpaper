f678b572ccf825c1a239338a0bae98a309e08160
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-04-06 19:39:04 +0300

    Add a helper script to set wallpaper

---
+++ set_binary_wallpaper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+base_dir=`dirname "$0"`
+
+$base_dir/venv/bin/python $base_dir/binary_clock_wallpaper.py
+
+## Use any wallpaper setter of your choice. I use nitrogen
+nitrogen --set-zoom-fill $base_dir/result.png
