b6aa40b0b76b38ed150ccfdf7e5ccc0aa90f38cb
Loïc Reynier <loic@loicreynier.fr>
2022-04-07 13:44:36 +0200

    Remove `venv` dependency in shell script

--- set_binary_wallpaper.sh
+++ set_binary_wallpaper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 base_dir=`dirname "$0"`
+if [ -f $base_dir/venv/bin/python ]; then
+    python=$base_dir/venv/bin/python
+else
+    python=python
+fi
 
-$base_dir/venv/bin/python $base_dir/binary_clock_wallpaper.py
+$python $base_dir/binary_clock_wallpaper.py
 
 ## Use any wallpaper setter of your choice. I use nitrogen
 nitrogen --set-zoom-fill $base_dir/img/result.png
