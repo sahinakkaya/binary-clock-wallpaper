356f3bb253ecc14997e1abaf7406b914f69f06dc
ahmed <57039938+HqNw@users.noreply.github.com>
2022-04-13 22:46:53 +0200

    Update set_binary_wallpaper.sh

--- set_binary_wallpaper.sh
+++ set_binary_wallpaper.sh
@@ -11,3 +11,5 @@ $python $base_dir/binary_clock_wallpaper.py "$@"
 
 ## Use any wallpaper setter of your choice. I use nitrogen
 nitrogen --set-zoom-fill $base_dir/img/result.png
+#comment the above line and uncomment this if you use sway
+#swaymsg -s /run/user/1000/sway-ipc.1000.*.sock output "*" bg $base_dir/img/result.png fill
